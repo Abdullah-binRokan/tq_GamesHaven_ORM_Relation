f214283794551ac6152775e461d9bf0209c859ec
AR <aarokan@gmail.com>
2024-11-19 13:00:20 +0300

    Add filters to admin interface

--- GamesHaven/games/admin.py
+++ GamesHaven/games/admin.py
@@ -6,9 +6,11 @@ from .models import Game, Review
 class GameAdmin(admin.ModelAdmin):
     # specify the fields to display in the list veiw of the admin interface
     list_display = ("title", "publisher", "rating")
+    list_filter = ("rating",)
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("name", "game", "rating")
+    list_filter = ("game", "rating")
 
 # register the models with the custom GameAdmin & ReviewAdmin configuration.
 admin.site.register(Game, GameAdmin)
