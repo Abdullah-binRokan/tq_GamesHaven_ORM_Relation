a391ea36f1cbaa2427d2993f592f4afa9564e990
AR <aarokan@gmail.com>
2024-11-23 00:54:27 +0300

    handle add publisher form

--- GamesHaven/publishers/views.py
+++ GamesHaven/publishers/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from .models import Publisher
+from .forms import PublisherForm
 
 # Create your views here.
 def add_publisher_view(request: HttpRequest):
+    if request.method == "POST":
+        publisher_form = PublisherForm(request.POST, request.FILES)
+        if publisher_form.is_valid():
+            publisher_form.save()
+            # return redirect("")
+        else:
+            print("form error: ", PublisherForm.errors)
 
     return render(request, "publishers/add.html")
