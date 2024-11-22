8e657e7fb438ee0cc395c09ba886943b6887fefe
AR <aarokan@gmail.com>
2024-11-22 22:53:31 +0300

    Create Publishers app & model

--- GamesHaven/GamesHaven/urls.py
+++ GamesHaven/GamesHaven/urls.py
@@ -22,5 +22,6 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls")),
-    path("games/", include("games.urls"))
+    path("games/", include("games.urls")),
+    path("publishers/", include("publishers.urls")),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
---
+++ GamesHaven/publishers/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=1024)
+    image = models.ImageField(upload_to="publishers_images/", default="publishers_images/default.jpg")
