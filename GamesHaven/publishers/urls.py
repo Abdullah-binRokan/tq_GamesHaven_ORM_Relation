54af9c28a21e8ae03890444f246e91bc474e5bff
AR <aarokan@gmail.com>
2024-11-23 18:49:59 +0300

    Create publishers detail page

--- GamesHaven/publishers/urls.py
+++ GamesHaven/publishers/urls.py
@@ -6,4 +6,5 @@ app_name = "publishers"
 urlpatterns = [
     path("add/", views.add_publisher_view, name="add_publisher_view"),
     path("all/", views.all_publishers_view, name="all_publishers_view"),
+    path("<publisher_id>/", views.detail_publisher_view, name="detail_publisher_veiw"),
 ]
--- GamesHaven/publishers/views.py
+++ GamesHaven/publishers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import Publisher
 from .forms import PublisherForm
+from games.models import Game
 
 # Create your views here.
 def add_publisher_view(request: HttpRequest):
@@ -20,3 +21,8 @@ def all_publishers_view(request: HttpRequest):
     publishers = Publisher.objects.all()
 
     return render(request, "publishers/all.html", {"publishers": publishers})
+
+def detail_publisher_view(request: HttpRequest, publisher_id: int):
+    publisher = Publisher.objects.get(pk=publisher_id)
+
+    return render(request, "publishers/detail.html", {"publisher": publisher})
